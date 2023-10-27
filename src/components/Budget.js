966b7d4f5148d8524644992f3453ebeba0bb4c27
kavyaramachandra <kavya.8794@gmail.com>
2023-10-27 02:50:25 -0400

    budget-1

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,17 @@
+import React, { useContext, useState } from 'react';
+import { AppContext } from '../context/AppContext';
 
+const Budget = () => {
+    const { budget } = useContext(AppContext);
+    const [newBudget, setNewBudget] = useState(budget);
+    const handleBudgetChange = (e) => {
+        setNewBudget(e.target.value);
+    }
+    return (
+<div className='alert alert-secondary'>
+<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
+<span>Budget: £{newBudget}</span>
+</div>
+    );
+};
+export default Budget;
--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -80,11 +80,11 @@ export const AppReducer = (state, action) => {
 const initialState = {
     budget: 2000,
     expenses: [
-        { id: "Marketing", name: 'Marketing', cost: 50 },
-        { id: "Finance", name: 'Finance', cost: 300 },
-        { id: "Sales", name: 'Sales', cost: 70 },
-        { id: "Human Resource", name: 'Human Resource', cost: 40 },
-        { id: "IT", name: 'IT', cost: 500 },
+        { id: "Marketing", name: 'Marketing', cost: 0 },
+        { id: "Finance", name: 'Finance', cost: 0 },
+        { id: "Sales", name: 'Sales', cost: 0 },
+        { id: "Human Resource", name: 'Human Resource', cost: 0 },
+        { id: "IT", name: 'IT', cost: 0 },
     ],
     currency: '£'
 };
